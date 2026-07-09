addd5db907fdcb516b8db4f51d3210635f10cea9
flyincloud2001 <flyincloud2001@gmail.com>
2026-07-09 18:14:05 +0800

    Auto sync: 2026-07-09 18:14:05

--- analysis/2026-07-05_walmart_customer_clustering/Walmart_KMeans_Unit_Price_Profit .py
+++ analysis/2026-07-05_walmart_customer_clustering/Walmart_KMeans_Unit_Price_Profit .py
@@ -99,17 +99,10 @@ def silhouette_score_line_plot(scaled_data):
 
 def interpretation(labels, data):
     data['cluster'] = labels
-    data.groupby('cluster')[FEATURES].mean()
-    
-    fig, ax = plt.subplots(figsize=(12, 5))
-    ax.plot(clusters, silhouette_scores, label='silhouette score')
-    ax.set_xlabel("k values")
-    ax.set_ylabel("silhouette score")
-    ax.set_title("k values versus silhouette scores")
-    ax.axvline(3, color='r', alpha=0.5, linestyle='--', label='Elbow Point')
-    plt.tight_layout()
-    plt.savefig("k_values_versus_silhouette_scores.png", dpi=150)
-    plt.show()
+    clusters = data.groupby('cluster')[FEATURES].mean()
+    print(clusters)
+
+
 
 
 data = load_data(FILE_PATH, FEATURES)
@@ -121,6 +114,7 @@ for i in range(2, 4):
     model, labels = fit_kmeans(scaled, i)
     plot_clusters(data, FEATURES, labels, i)
 
+interpretation(labels, data)
 
 print(data)
 print(labels)
