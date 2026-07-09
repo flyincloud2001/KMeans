419fb72b8a113889f08ce8a45b9ab8af507e53cf
flyincloud2001 <flyincloud2001@gmail.com>
2026-07-09 15:37:49 +0800

    Auto sync: 2026-07-09 15:37:49

--- analysis/2026-07-05_walmart_customer_clustering/Walmart_KMeans_UnitPrice_Profit (3).py
+++ analysis/2026-07-05_walmart_customer_clustering/Walmart_KMeans_UnitPrice_Profit (3).py
@@ -59,4 +59,5 @@ plot_clusters(data, FEATURES, labels)
 
 # ------- 用來算出inertia的function ----------------
 def inertia_calculation(data, model, n):
-    centroids = data[FEATURES] - 
+    centroids = model.centroid
+    np.norm(data[FEATURES] - 
