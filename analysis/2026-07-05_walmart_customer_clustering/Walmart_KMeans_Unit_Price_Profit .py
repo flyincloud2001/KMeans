04b376bb62c109d8451275746c4099585b0a4cfc
flyincloud2001 <flyincloud2001@gmail.com>
2026-07-09 18:23:12 +0800

    Auto sync: 2026-07-09 18:23:12

--- analysis/2026-07-05_walmart_customer_clustering/Walmart_KMeans_Unit_Price_Profit .py
+++ analysis/2026-07-05_walmart_customer_clustering/Walmart_KMeans_Unit_Price_Profit .py
@@ -106,15 +106,15 @@ def bar_plot(data):
 
     fig, ax = plt.subplots(figsize=(8, 5))
     data.groupby('cluster')[FEATURES].mean()['Profit'].plot(kind='bar', ax=ax)
-    ax.set_title('各群平均獲利')
+    ax.set_title('average profit for each cluster')
     ax.set_xlabel('cluster')
     ax.set_ylabel('Profit')
     plt.tight_layout()
     plt.show()
 
     fig, ax = plt.subplots(figsize=(8, 5))
-    data.groupby('cluster').size()['Profit'].plot(kind='bar', ax=ax)
-    ax.set_title('各群平均獲利')
+    data.groupby('cluster').size().plot(kind='bar', ax=ax)
+    ax.set_title('')
     ax.set_xlabel('cluster')
     ax.set_ylabel('Profit')
     plt.tight_layout()
